ceb4a4884d759af40682efd3767bcca48e6e3706
Divy Srivastava <dj.srivastava23@gmail.com>
2019-05-21 15:16:43 +0530

    Add cli initialization

--- cli.js
+++ cli.js
@@ -1,5 +1,48 @@
 const vorpal = require('vorpal')();
-var bloomConfig = require('./_data/config');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+var configUrl = path.join(__dirname, './_data/config.json');
+
+var initQuestion = [
+  {
+    type:'input',
+    name:'title',
+    message:'Enter a title'
+  },
+  {
+    type:'input',
+    name:'website',
+    message:'Enter you blog url'
+  },
+  {
+    type:'input',
+    name:'author',
+    message:'Enter you name'
+  },
+  {
+    type:'input',
+    name:'host',
+    message:'Enter your host name'
+  },
+  {
+    type:'input',
+    name:'port',
+    message:'Enter a port for the server'
+  },
+  {
+    type:'input',
+    name:'username',
+    message:'Enter a username for accessing console'
+  },
+  {
+    type:'input',
+    name:'password',
+    message:'Enter a password for console'
+  }
+]
+
 vorpal
   .command('say [words...]')
   .option('-b, --backwards')
@@ -16,11 +59,11 @@ vorpal
 vorpal
   .command('init')
   .action(function (args, callback) {
-    inquirer.prompt(questions).then(answers => {
-      console.log(`Hi ${answers['name']}!`)
+    inquirer.prompt(initQuestion).then(answers => {
+      fs.writeFileSync(configUrl, JSON.stringify(answers, null, 2));
+      this.log('Done!');
+      callback();
     })
-    callback();
   });
 
-
-vorpal.show()
+vorpal.delimiter('bloom$').show()
